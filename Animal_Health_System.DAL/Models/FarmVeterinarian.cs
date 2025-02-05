6e6839a50ed4b040a76339ed9be34fa0469bf2a4
sohaibaltrefe <sohaibshaltafaltrefe1@gmail.com>
2025-02-05 09:21:22 +0300

    zz

--- Animal_Health_System.DAL/Models/FarmVeterinarian.cs
+++ Animal_Health_System.DAL/Models/FarmVeterinarian.cs
@@ -8,17 +8,17 @@ namespace Animal_Health_System.DAL.Models
 {
     public class FarmVeterinarian : EntityBase
     {
-        public int  Id { get; set; }
-        
+        public int Id { get; set; }
+
         public string Name { get; set; }
-        
+
         public int? FarmId { get; set; }
-        public Farm Farm  { get; set; }
+        public Farm Farm { get; set; }
 
 
-      
+
 
         public int? VeterinarianId { get; set; }
-        public Veterinarian Veterinarian  { get; set; }
+        public Veterinarian Veterinarian { get; set; }
     }
 }
